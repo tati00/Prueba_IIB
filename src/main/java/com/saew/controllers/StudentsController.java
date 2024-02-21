1296efb60e656b4a2338b08d1223e4d2dacdd468
Erika Anrrango <erika.anrrango@epn.edu.ec>
2024-02-21 17:25:01 -0500

    prueba

--- src/main/java/com/saew/controllers/StudentsController.java
+++ src/main/java/com/saew/controllers/StudentsController.java
@@ -1,11 +1,19 @@
 package com.saew.controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saew.dto.Student;
@@ -13,43 +21,52 @@ import com.saew.dto.Student;
 @RestController
 @RequestMapping("/students")
 public class StudentsController {
-    
-    
-    public List<Student> getAllStudents() {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
-    }
-    
+    private List<Student> students;
 
-    
-    public List<Student> getStudentByName(String name) {    
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+    public List<Student> agregarEstudiantes() {
+        students.add(new Student(1, "Erika", "erika@epn.edu.ec", 9.55f));
+        students.add(new Student(1, "Luis", "luis@epn.edu.ec", 9.85f));
+        students.add(new Student(1, "Jeremy", "jeremy@epn.edu.ec", 5.5f));
+        students.add(new Student(1, "Aislyn", "aislyn@epn.edu.ec", 6.83f));
+        return students;
     }
-       
-    public ResponseEntity<Student> getStudentById(String id) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @GetMapping("/getStudentByName/{name}")
+    public List<Student> getStudentByName(@RequestParam String name) {
+        return students.stream().filter(student -> student.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/getStudentById/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
+        Optional<Student> optStudents = students.stream().filter(student -> student.getId().equals(id)).findFirst();
+        if (optStudents.isPresent()) {
+            return ResponseEntity.ok(optStudents.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @PostMapping
     public ResponseEntity<Student> createStudent(Student student) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+        students.add(student);
+        return ResponseEntity.ok(student);
     }
 
     @PatchMapping
     public ResponseEntity<Student> updateProduct(Student product) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();   
+        students.add(product);
+        return ResponseEntity.ok(product);
     }
-    
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<Student> deleteStudent(String id) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();   
+        students.remove(id);
+        return ResponseEntity.ok(students.remove(id));
     }
 
-    
-
-    
 }
--- src/main/java/com/saew/dto/Student.java
+++ src/main/java/com/saew/dto/Student.java
@@ -1,5 +1,29 @@
 package com.saew.dto;
 
 public class Student {
-    //TODO: Add the missing fields
+    private int id;
+    private String name;
+    private String correo;
+    private Float gpa;
+    public Student(int id, String name, String correo, Float gpa) {
+        this.id = id;
+        this.name = name;
+        this.correo = correo;
+        this.gpa = gpa;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
+
+
